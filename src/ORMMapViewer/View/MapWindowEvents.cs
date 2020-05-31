3524b47425b5510567782d722ad426e5de8027cd
pickar <sereges2@gmail.com>
2020-05-31 21:11:39 +0300

    Small control refactoring

--- src/ORMMapViewer/View/MapWindowEvents.cs
+++ src/ORMMapViewer/View/MapWindowEvents.cs
@@ -2,6 +2,8 @@
 using ORMMap.VectorTile.Geometry;
 using System;
 using System.Windows;
+using System.Windows.Media.Media3D;
+using System.Windows.Input;
 
 namespace ORMMapViewer
 {
@@ -11,17 +13,17 @@ namespace ORMMapViewer
         bool mouseDown;
         Vector2<int> oldPos = new Vector2<int>(0, 0);
 
-        private void Window_MouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
+        private void Window_MouseWheel(object sender, MouseWheelEventArgs e)
         {
             zoom += e.Delta / (50 * zoom);
             zoom = Math.Min(Math.Max(zoom, 1), 20);
-            camera.Position = new System.Windows.Media.Media3D.Point3D(camera.Position.X, camera.Position.Y, 5041.23 * (21 - zoom));
+            camera.Position = new Point3D(camera.Position.X, camera.Position.Y, 5041.23 * (21 - zoom));
             this.Title = $"ORMMap [Zoom: {zoom}]";
 
             //UpdateScene();
         }
 
-        private void Window_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (!mouseDown)
             {
@@ -31,16 +33,14 @@ namespace ORMMapViewer
             }
         }
 
-        private void Window_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e) => mouseDown = false;
+        private void Window_MouseUp(object sender, MouseButtonEventArgs e) => mouseDown = false;
 
-        private void Window_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        private void Window_MouseMove(object sender, MouseEventArgs e)
         {
             if (!mouseDown) return;
             
             Point pos = e.GetPosition(this);
-            Console.WriteLine(pos + ", " + oldPos);
-            camera.Position = new System.Windows.Media.Media3D.Point3D(camera.Position.X + (pos.X - oldPos.X)*20, camera.Position.Y + (pos.Y - oldPos.Y)*20, camera.Position.Z);
-            Console.WriteLine((pos.X - oldPos.X) + ", " + (pos.Y - oldPos.Y));
+            camera.Position = new Point3D(camera.Position.X - (pos.X - oldPos.X)*(200/zoom), camera.Position.Y + (pos.Y - oldPos.Y)*(200/zoom), camera.Position.Z);
             oldPos.set((int) pos.X, (int) pos.Y);
         }
     }
